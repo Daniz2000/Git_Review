891c61a3fd17f5fab0d1a64e26d4f0043ff499b1
Daniz2000 <m_daniz2000@yahoo.com>
2022-12-28 19:56:23 -0800

    Do some changes

--- src/Daniz/Testloginfunction.java
+++ src/Daniz/Testloginfunction.java
@@ -2,6 +2,6 @@ package Daniz;
 
 public class Testloginfunction {
     public static void main(String[] args) {
-        System.out.println("I'm testing log in");
+        System.out.println("I'm testing log in in cell phone");
     }
 }
